0ad7e7d5b4e6b7b3e6080bca73eab15d209df467
amr <amr442028@gmail.com>
2024-03-20 21:42:23 +0200

    update suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,59 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppLayout from "./pages/applayout/AppLayout";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Team from "./pages/team/Team";
-import PageNotFound from "./pages/undefined/PageNotFound";
-import Calendar from "./pages/calendar/Calendar";
-import Faq from "./pages/faq/Faq";
-import Form from "./pages/form/Form";
-import Geography from "./pages/geography/Geography";
-import BarChart from "./pages/barChart/BarChart";
-import Contacts from "./pages/contacts/Contacts";
-import Invoices from "./pages/invoices/Invoices";
-import LineChart from "./pages/lineChart/LineChart";
-import PieChart from "./pages/pieChart/PieChart";
-import { useState } from "react";
+
+import { Suspense, lazy, useState } from "react";
+const AppLayout = lazy(() => import("./pages/applayout/AppLayout"));
+const Dashboard = lazy(() => import("./pages/dashboard/Dashboard"));
+const Team = lazy(() => import("./pages/team/Team"));
+const PageNotFound = lazy(() => import("./pages/undefined/PageNotFound"));
+const Calendar = lazy(() => import("./pages/calendar/Calendar"));
+const Faq = lazy(() => import("./pages/faq/Faq"));
+const Form = lazy(() => import("./pages/form/Form"));
+const Geography = lazy(() => import("./pages/geography/Geography"));
+const BarChart = lazy(() => import("./pages/barChart/BarChart"));
+const Contacts = lazy(() => import("./pages/contacts/Contacts"));
+const Invoices = lazy(() => import("./pages/invoices/Invoices"));
+const LineChart = lazy(() => import("./pages/lineChart/LineChart"));
+const PieChart = lazy(() => import("./pages/pieChart/PieChart"));
+
 function App() {
   const [isDashboard, setIsDashboard] = useState(false);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route
-            index
-            element={
-              <Dashboard
-                setIsDashboard={setIsDashboard}
-                isDashboard={isDashboard}
-              />
-            }
-          />
-          <Route path="team" element={<Team />} />
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="bar" element={<BarChart isDashboard={isDashboard} />} />
-          <Route path="calendar" element={<Calendar />} />
-          <Route path="contacts" element={<Contacts />} />
-          <Route path="faq" element={<Faq />} />
-          <Route
-            path="geography"
-            element={<Geography isDashboard={isDashboard} />}
-          />
-          <Route path="form" element={<Form />} />
-          <Route path="invoices" element={<Invoices />} />
-          <Route path="line" element={<LineChart />} />
-          <Route path="pie" element={<PieChart isDashboard={isDashboard} />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<h1>loading.....</h1>}>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route
+              index
+              element={
+                <Dashboard
+                  setIsDashboard={setIsDashboard}
+                  isDashboard={isDashboard}
+                />
+              }
+            />
+            <Route path="team" element={<Team />} />
+            <Route path="*" element={<PageNotFound />} />
+            <Route
+              path="bar"
+              element={<BarChart isDashboard={isDashboard} />}
+            />
+            <Route path="calendar" element={<Calendar />} />
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="faq" element={<Faq />} />
+            <Route
+              path="geography"
+              element={<Geography isDashboard={isDashboard} />}
+            />
+            <Route path="form" element={<Form />} />
+            <Route path="invoices" element={<Invoices />} />
+            <Route path="line" element={<LineChart />} />
+            <Route
+              path="pie"
+              element={<PieChart isDashboard={isDashboard} />}
+            />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
