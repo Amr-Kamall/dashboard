cdcb03c8dccec583d2fe79b54d7f6e6fb3c63966
amr <amr442028@gmail.com>
2024-04-02 05:07:13 +0200

    update userName

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -10,20 +10,22 @@ function UserAvatar() {
     queryFn: getCurrentUser,
   });
 
+  console.log(getCurrentUser);
+
   if (isLoading) return <Spinner width={"small"} />;
   if (!userData || !userData.user_metadata) {
     return null;
   }
 
-  const { fullName, avatar } = userData.user_metadata;
+  const { firstName, avatar } = userData.user_metadata;
   return (
     <Stack direction={"row"} alignItems={"center"} gap={1}>
       <img
         src={avatar || "/default-user.jpg"}
         className="img-avatar"
-        alt={fullName}
+        alt={firstName}
       />
-      <Typography>{fullName}</Typography>
+      <Typography>{firstName}</Typography>
     </Stack>
   );
 }
